07bec3ce362475e361ed74ac41622cf1dd105b95
desmo <62178867+desmodrone@users.noreply.github.com>
2025-06-26 06:35:50 +0200

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    country: Option<String>,
+}
+
 mod theme;
 
 use dotenv::dotenv;
@@ -14,20 +23,28 @@ fn render_articles(articles: &Vec<Article>) {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv();
-
-    let api_key = std::env::var("API_KEY")?;
-
-    let mut newsapi = NewsAPI::new(&api_key);
+'''async fn fetch_news(country: Country, api_key: &str) -> Result<Vec<Article>, Box<dyn Error>> {
+    let mut newsapi = NewsAPI::new(api_key);
     newsapi
         .endpoint(Endpoint::TopHeadlines)
-        .country(Country::Us);
+        .country(country);
 
-    // let newsapi_response = newsapi.fetch_async().await?;
-    let newsapi_response = newsapi.fetch()?;
-    render_articles(&newsapi_response.articles());
+    let newsapi_response = newsapi.fetch_async().await?;
+    Ok(newsapi_response.articles().to_vec())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    dotenv().ok();
+
+    let api_key = std::env::var("API_KEY").map_err(|_| "Failed to get API_KEY. Make sure you have a .env file with an API_KEY variable".to_string())?;
+
+    let country = args.country.map(|s| Country::from_str(&s)).transpose()?.unwrap_or(Country::Us);
+
+    let articles = fetch_news(country, &api_key)?;
+    render_articles(&articles);
 
     Ok(())
-}
+}'''
